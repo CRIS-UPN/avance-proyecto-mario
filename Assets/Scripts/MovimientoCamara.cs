e99638048b96f839f99cb4afa16a8c538dd15901
Cristhian Ramos <166570071+CRIS-UPN@users.noreply.github.com>
2026-05-26 11:49:17 -0500

    movimiento de camara

---
+++ Assets/Scripts/MovimientoCamara.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class MovimientoCamara : MonoBehaviour
+{
+    public Transform target;
+    public Transform leftBound, rightBound;
+    private float leftBoundsWidth, rightBoundsWidth;
+
+    public float smoothDampTime = 0.15f;
+    private Vector3 smoothDampVelocity = Vector3.zero;
+    private float camWidth, camHeight, levelMinX, levelMaxX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        camHeight = Camera.main.orthographicSize * 2;
+        camWidth = camHeight * Camera.main.aspect;
+
+        leftBoundsWidth = leftBound.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+        rightBoundsWidth = rightBound.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+
+        levelMinX = leftBound.position.x + leftBoundsWidth + (camWidth / 2);
+        levelMaxX = rightBound.position.x - rightBoundsWidth - (camWidth / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float targetX = Mathf.Max(levelMinX, Mathf.Min(levelMaxX, target.position.x));
+        float x = Mathf.SmoothDamp(transform.position.x, targetX, ref smoothDampVelocity.x, smoothDampTime);
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+}
